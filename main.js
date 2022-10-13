b0bf49f8cdcce93ed694a9e252430920373d4c90
vderbier <victor.derbier@gmail.com>
2022-10-13 12:04:39 -0700

    Update main.js
    cleaned up commented code

--- main.js
+++ main.js
@@ -17,16 +17,7 @@ characters = [
 l l
  r
 l l
-`,
-  `
-llll
-`,
-  `
-lll
-`,
-  `
-lll
-`,
+`
 ];
 
 options = {
@@ -35,23 +26,9 @@ options = {
   seed: 15,
 };
 
-// /**
-//  * @type {{
-//  * pos: Vector, angle: number, speed: number,
-//  * shotTicks: number, burstTicks: number, burstCount: number
-//  * turnTicks: number,
-//  * isReflecting: boolean
-//  * }[]}
-//  */
-// let enemies;
-// /** @type {{pos: Vector, width: number, angle: number}[]} */
-// // let walls;
-// /** @type {{pos: Vector, angle: number, range: number, side: "player" | "enemy"}[]} */
-// let bullets;
 /**
  * @type {{
- * pos: Vector, angle: number, speed: number,
- * isReflecting: boolean, shotTicks: number
+ * pos: Vector, angle: number
  * }}
  */
 let player;
@@ -61,23 +38,8 @@ let vel;
 let angle;
 
 let level;
-let isLevelCleared;
-let levelClearTicks;
-// /** @type {{pos:Vector, radius: number}} */
-// let nextCircleTarget;
-// /** @type {{pos:Vector, radius: number}} */
-// let nextCircle;
 /** @type {{pos:Vector, radius: number}} */
 let circle;
-// let circleTicks;
-// let multiplier;
-const angleVels = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1],
-];
-const shownRange = 30;
 
 function update() {
   if (!ticks) {
@@ -87,64 +49,13 @@ function update() {
 
     vel = vec(0.1, 0);
     angle = 0;
-  }
-  if (isLevelCleared) {
-    //circleTicks = 99;
-    //circle = { pos: vec(50, 50), radius: 120 };
-    // nextCircle = { pos: vec(50, 50), radius: 60 };
-    // nextCircleTarget = { pos: vec(), radius: 0 };
+
     player = {
       pos: vec(50, 10),
-      angle: 3,
-      speed: 0,
-      isReflecting: false,
-      shotTicks: 0,
+      angle: 3
     };
-    level++;
-    isLevelCleared = false;
-    multiplier = 1;
   }
-  // circleTicks--;
-  // if (circleTicks === 9) {
-  //   nextCircleTarget.radius = nextCircle.radius - 10;
-  //   nextCircleTarget.pos.set(nextCircle.pos);
-  //   for (let i = 0; i < 99; i++) {
-  //     nextCircleTarget.pos.set(rnd(10, 90), rnd(10, 90));
-  //     if (
-  //       nextCircleTarget.pos.distanceTo(nextCircle.pos) <
-  //       nextCircle.radius - nextCircleTarget.radius
-  //     ) {
-  //       break;
-  //     }
-  //   }
-  // }
-  // if (circleTicks < 0) {
-  //   circleTicks = 600;
-  // }
-  // if (circleTicks < 9) {
-  //   nextCircle.pos.add(
-  //     (nextCircleTarget.pos.x - nextCircle.pos.x) / (circleTicks + 1),
-  //     (nextCircleTarget.pos.y - nextCircle.pos.y) / (circleTicks + 1)
-  //   );
-  //   nextCircle.radius +=
-  //     (nextCircleTarget.radius - nextCircle.radius) / (circleTicks + 1);
-  // }
-  // if (nextCircle.radius < 60) {
-  //   // setting outer circle
-  //   color("light_black");
-  //   arc(50, 50, 50, 3);
-  //   // if statement below deals with shrinking the blue circle
-  //   if (circleTicks > 9) {
-  //     circle.pos.add(
-  //       (nextCircle.pos.x - circle.pos.x) / circleTicks,
-  //       (nextCircle.pos.y - circle.pos.y) / circleTicks
-  //     );
-  //     circle.radius += (nextCircle.radius - circle.radius) / circleTicks;
-  //   }
-  //   // setting inner circle
-  //   color("blue");
-  //   arc(50, 50, 30, 3);
-  // }
+  
   const center = vec(50, 50);
   const cr = 15;
 
@@ -153,6 +64,7 @@ function update() {
   arc(center, 50, 3);
   // setting inner circle
   arc(center, 30, 3);
+
   // draw character
   color("black");
   const c = char("a", player.pos);
@@ -160,7 +72,7 @@ function update() {
   if (input.isPressed){
     vel.mul(0.7);                        // slow down 
     vel.addWithAngle(angle, 0.1);        // turn
-    angle = turnTo(angle, center, 0.02); // towards the center of the screen
+    angle = turnTo(angle, center, 0.02* sqrt(difficulty)); // towards the center of the screen
     color("light_red");
     particle(player.pos, 3, 1, center.angleTo(player.pos), 0.4);
   }
@@ -170,90 +82,9 @@ function update() {
   // update player pos
   player.pos.add(vel.x * difficulty * 1.2, vel.y * difficulty* 1.2); 
 
-  // supposed to reflect player sprite? not working 
-  // if (player.pos.x < 0) {
-  //   reflect(0);
-  // }
-  // if (player.pos.x > 99) {
-  //   reflect(PI);
-  // }
-  // if (player.pos.y < 0) {
-  //   reflect(PI / 2);
-  // }
-  // if (player.pos.y > 99) {
-  //   reflect(-PI / 2);
-  // }
-  
   // game over  
   if (player.pos.distanceTo(center) < 30 || player.pos.distanceTo(center) > 50) {
     end();
-  }
-
-  //player.pos.add(av[0] * player.speed, av[1] * player.speed);
-
-  // checkCircleReflect(player);
-  // color("black");
-  // char("b", player.pos.x + av[0] * 2, player.pos.y + av[1] * 2, {
-  //   rotation: player.angle,
-  // });
-  // color("blue");
-  // const c = char("a", player.pos, { rotation: player.angle }).isColliding;
-  // if (
-  //   c.char.c ||
-  //   circle.radius < 1 ||
-  //   player.pos.distanceTo(circle.pos) > circle.radius * 1.05
-  // ) {
-  //   play("lucky");
-  //   end();
-  // }
-  //player.pos.clamp(0, 99, 0, 99);
-
-  // if (c.rect.yellow || !player.pos.isInRect(0, 0, 99, 99)) {
-  //   if (!player.isReflecting) {
-  //     player.angle += 2;
-  //     player.isReflecting = true;
-  //   }
-  // } else {
-  //   player.isReflecting = false;
-  // }
-
-  //player.angle = wrap(player.angle, 0, 4);
-
-  // if (levelClearTicks <= -60 && enemies.length === 0) {
-  //   play("powerUp");
-  //   levelClearTicks = 60;
-  // }
-  // if (levelClearTicks > 0) {
-  //   color("black");
-  //   text("WINNER!", 30, 50);
-  //   levelClearTicks--;
-  //   if (levelClearTicks === 0) {
-  //     isLevelCleared = true;
-  //   }
-  // } else if (levelClearTicks > -60) {
-  //   color("black");
-  //   text(`LEVEL ${level}`, 30, 50);
-  //   levelClearTicks--;
-  // }
-
-  /*
-  checks if player is colliding with the blue circle
-  if collides flip player 180 degrees, creating a bounce back in the opposite direction 
-  */
-  function checkCircleReflect(o) {
-    if (o.pos.distanceTo(circle.pos) < circle.radius) {
-      return;
-    }
-    const a = o.pos.angleTo(circle.pos);
-    if (a < (-PI / 4) * 3 || a > (PI / 4) * 3) {
-      o.angle = 2;
-    } else if (a > PI / 4) {
-      o.angle = 1;
-    } else if (a < -PI / 4) {
-      o.angle = 3;
-    } else {
-      o.angle = 0;
-    }
   }
 
   function turnTo(a, p, v) {
@@ -266,17 +97,6 @@ function update() {
     } else {
       return a + v;
     }
-  }
-
-  function reflect(n) {
-    const r = wrap(angle + PI - n, -PI, PI);
-    if (abs(r) < PI / 2) {
-      angle = angle + PI - r * 2;
-    }
-    const s = vel.length;
-    vel.set(0, 0).addWithAngle(angle, s / 2);
-    play("select");
-    addingScore = 1;
   }
 }
 addEventListener("load", onLoad);
